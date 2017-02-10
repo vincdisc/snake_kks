2e8b2f934b4b34114a2a74e01a22e39a29c92dc8
vincent <vincent.dischl@hotmail.ch>
2017-02-10 13:39:47 +0100

    erste Schritte

--- snake.py
+++ snake.py
@@ -15,14 +15,24 @@ class Fruits:
 
 class  Snake:
     def __init__(self):
-        self.snake={'x':300,'y':300, 'dx':0, 'dy':0}
+        self.snake={'x':1,'y':1, 'dx':0, 'dy':0}
 
 class Spielfeld:
     def __init__(self,groesse):
         self.felder = [[0 for j in range(groesse)] for i in range(groesse)]
 
+        for i in range(1, groesse - 1):
+            self.felder[i][i] = 1
+
+        for i in range(groesse):
+            self.felder[0][i] = 1
+            self.felder[groesse - 1][i] = 1
+            self.felder[i][0] = 1
+            self.felder[i][groesse - 1] = 1
+
 
 def makeGUI():
+
 
     BOARD_LENGHT = 600
     BOARD_HIGHT = BOARD_LENGHT
@@ -31,6 +41,13 @@ def makeGUI():
 
     assert BOARD_LENGHT%CELLSIZE==0
     assert BOARD_HIGHT%CELLSIZE==0
+    CELLWIDTH=int(BOARD_LENGHT/CELLSIZE)
+    CELLHEIGHT=int(BOARD_HIGHT/CELLSIZE)
+
+    my_feld=Spielfeld(CELLWIDTH)
+
+    snake=Snake()
+
 
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
@@ -39,4 +56,51 @@ def makeGUI():
     pygame.display.set_caption('SNAKE')
 
 
+    while True:
+        DISPLAYSURF.fill(WHITE)
 
+        for event in pygame.event.get():
+
+            if (event.type==pygame.KEYUP and event.key==pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            elif event.type==pygame.KEYDOWN:
+                #snake
+                if event.key==pygame.K_w:
+                    if snake["dx"]==0:
+                        snake["dx"]=-1
+                    elif snake["dx"]==1:
+                        snake["dx"]==0
+                elif event.key==pygame.K_s:
+                    if snake["dx"]==-1:
+                        snake["dx"]=0
+                    elif snake["dx"]==0:
+                        snake["dx"]=1
+                elif event.key==pygame.K_a:
+                    if snake["dy"]==0:
+                        snake["dy"]=-1
+                    elif snake["dy"]==1:
+                        snake["dy"]=0
+                elif event.key==pygame.K_d:
+                    if snake["dy"]==-1:
+                        snake["dy"]=0
+                    elif snake["dy"]==0:
+                        snake["dy"]=1
+
+
+
+
+        for x in range(0,BOARD_LENGHT,CELLSIZE):
+            pygame.draw.line(DISPLAYSURF,BLACK,(x,0),(x,BOARD_LENGHT))
+
+        for y in range(0,BOARD_HIGHT,CELLSIZE):
+            pygame.draw.line(DISPLAYSURF,BLACK,(0,y),(BOARD_HIGHT,y))
+
+
+
+
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+if __name__=="__main__":
+    makeGUI()
